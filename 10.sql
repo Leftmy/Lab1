cdd02af7638b43c8e45d752a56dc51e8f523b36b
Leftmy <161228886+Leftmy@users.noreply.github.com>
2024-02-26 10:38:51 +0200

    Delete 10.sql

--- 10.sql
+++
@@ -1,4 +0,0 @@
-select first_name, last_name, extract(year from now()) - extract(year from birth_date)
-from employees
-order by extract(year from now()) - extract(year from birth_date) desc
-limit 3;
